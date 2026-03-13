6b23ea511706eff9924583e3b5fe25f5172843fe
Keegan Miller <keegan.miller@hotmail.com>
2026-03-13 11:34:27 -0400

    Commented out non-functional masking line in Ubuntu wazuh agent script

--- Linux Wazuh Setup/ECOM/Agent_deb.sh
+++ Linux Wazuh Setup/ECOM/Agent_deb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo systemctl mask wazuh-agent
+#sudo systemctl mask wazuh-agent
 wget https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.14.3-1_amd64.deb && sudo WAZUH_MANAGER='172.20.242.20' WAZUH_AGENT_GROUP='default' WAZUH_AGENT_NAME='Ecom' dpkg -i ./wazuh-agent_4.14.3-1_amd64.deb && \
 sudo cp ossec.conf /var/ossec/etc/ossec.conf && \
 sudo chown root:wazuh /var/ossec/etc/ossec.conf && \
